a77c4e71aa0c3b8585a5b883fea66f859dd7a1e0
Emirhan Güven <e.guven@hotmail.com.tr>
2021-01-23 17:35:52 +0300

    Add project files.

---
+++ Edu/MyDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu
+{
+    class MyDictionary<T>
+    {
+        T[] names;
+
+        public MyDictionary()
+        {
+            names = new T[0];
+        }
+
+        public void Add(T name)
+        {
+            T[] tempNames = names;
+            names = new T[names.Length + 1];
+
+            for (int i = 0; i < tempNames.Length; i++)
+            {
+                names[i] = tempNames[i];
+            }
+
+            names[names.Length - 1] = name;
+        }
+
+        public void List()
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
---
+++ Edu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> Name = new MyDictionary<string>();
+            Name.Add("Emirhan");
+            Name.Add("Cansın");
+            Name.Add("Sude");
+            Name.Add("Bryan Adams");
+            Name.Add("LeBron James");
+            Name.Add("Stephen Curry");
+            Name.Add("Erich von Däniken");
+            Name.Add("Elon Musk");
+
+            Name.List();
+            Console.ReadLine();
+        }
+    }
+}
